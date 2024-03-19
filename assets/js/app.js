b5b9682d0a75500b28a07a9e67e5cff4edf56460
nicodellolio <151004233+nicodellolio@users.noreply.github.com>
2024-03-19 16:19:36 +0100

    init commit - add vue - add axios - print elements

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+const { createApp } = Vue
+createApp({
+    data() {
+        return {
+            emailList: []
+        }
+    },
+    mounted() {
+        for (let i = 1; i <= 10; i++) {
+            axios
+            .get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((response) => {
+                response.data = response.data.response
+                this.emailList.push(response.data)
+            })
+        }
+        console.log(this.emailList);
+    }
+}).mount('#app')
